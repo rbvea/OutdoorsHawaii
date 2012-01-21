9abbdfea3cfafdd5f4faa7b6a455534663117cb9
Russell Vea <rbvea@hawaii.edu>
2012-01-21 02:59:04 -1000

    pushing query

--- query.js
+++ query.js
@@ -8,42 +8,46 @@ var lanai = new google.maps.LatLng(20.81660,-156.92732);
 var maui = new google.maps.LatLng(20.79836,-156.33193);
 var hawaii = new google.maps.LatLng(19.55461,-155.51147);
 
-var islands = new Array("oahu", "kauai"); 
 
-var overlay = new Array("beaches", "trails"); 
-var view = new Array(2);
-
-function foo() {
-  var x = google.maps.mapTypeId.HYBRID;
-  alert(x);
-}
+var beaches = new Array(6);
+var trails = new Array(6);
 
 function initialize() 
 {
 
-        var myOptions = {
-		center: new google.maps.LatLng(21.438912,-158.000056),
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.HYBRID
-        };
-        map = new google.maps.Map(document.getElementById("map_canvas"),
-            myOptions);
+    var isles = new Array("kauai", "oahu", "molokai", "lanai", "maui", "hawaii");
+    var myOptions = {
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+			      myOptions);
 
-    for(var v = 0; v < overlay.length; v++) {
-	for(var i = 0; i < view.length; i++)  {
-	    var q =  "SELECT * FROM " + overlay[v] + " WHERE island='" + islands[i] + "'";
-	    $("body").append(overlay[v] + " " + islands[i]);
+     for(var i = 0; i < isles.length; i++)  {
+	var q =  "SELECT * FROM beaches WHERE island='" + isles[i] + "'";
+	beaches[i] = new google.maps.CartoDBLayer({
+	    map_canvas: 'map_canvas',
+	    map: map,
+	    user_name:"outdoorshawaii",
+	    table_name: "beaches",
+	    query: q,
+	    map_style: false,
+	    infowindow: false,
+	    auto_bound: true
+	});
+    }
 
-	    this.view[i] = new google.maps.CartoDBLayer({
-		map_canvas: 'map_canvas',
-		map: map,
-		user_name:"outdoorshawaii",
-		table_name: overlay[v],
-		query: q,
-		map_style: false,
-		infowindow: true,
-		auto_bound: true
-	    });
-	}
+    for(var i = 0; i < isles.length; i++)  {
+	var q =  "SELECT * FROM trails WHERE island='" + isles[i] + "'";
+	trails[i] = new google.maps.CartoDBLayer({
+	    map_canvas: 'map_canvas',
+	    map: map,
+	    user_name:"outdoorshawaii",
+	    table_name: "trails",
+	    query: q,
+	    map_style: false,
+	    infowindow: false,
+	    auto_bound: true
+	});
     }
 }
