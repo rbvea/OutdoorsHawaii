c6a45bc7edbf1be4b47ce3f12caa63af2765da26
Tony Gaskell <thgaskell@gmail.com>
2012-01-21 04:07:38 -1000

    pushing for more debugging

--- action.js
+++ action.js
@@ -1,40 +1,40 @@
-function toggle(obj){
+function toggle(obj) {
     $(obj).toggleClass("selected");
+    var b =  document.getElementById("beaches").checked;
+    var t =  document.getElementById("trails").checked;
     if( $(obj).text() == "All Islands") {
-	var fo =  document.getElementsByName("beaches").checked;
-	var of =  document.getElementsByName("trails").checked;
-	if (document.getElementsByName("beaches").checked) {
-	    for (var i = 0; i < beaches.length; i++) {
-		if (fo) {
-		    beaches[i].show();
-		} else {
-		    beaches[i].hide();
-		}
-	    }
-	    
-	    if (document.getElementsByName("trails").checked) {
-		for (var i = 0; i < trails.length; i++) {
-		    if (fo) {
-			trails[i].show();
-		    } else {
-			trails[i].hide();
-		    }
-		}
-	    }
-	}
-	else {
-	    $("#islands .tabs").each(function(i, checked) {
-		if( $(checked).hasClass("selected")) {
-		    if(document.getElementsByName("beaches").checked) {
-			beaches[i].show();
-		    }
-		    if(document.getElementsByName("trails").checked) {
-			trails[i].show();
-		    }
-		} else {
-		    beaches[i].hide();
-		    trails[i].hide();
-		}
-	    });
-	}
+        for (var i = 0; i < beaches.length; i++) {
+            if (b) {
+                beaches[i].show();
+            }
+            else {
+                beaches[i].hide();
+            }
+            if (t) {
+                trails[i].show();
+            }
+            else {
+                trails[i].hide();
+            }
+        }
+        $("#islands .tabs")..each(function(i, checked) {
+            alert(checked);
+            //$(checked).removeClass("selected");
+        });
     }
+    else {
+        $("#islands .tabs").each(function(i, checked) {
+            if( $(checked).hasClass("selected")) {
+                if(document.getElementById("beaches").checked) {
+                    beaches[i].show();
+                }
+                if(document.getElementById("trails").checked) {
+                    trails[i].show();
+                }
+            } else {
+                beaches[i].hide();
+                trails[i].hide();
+            }
+        });
+    }
+}
