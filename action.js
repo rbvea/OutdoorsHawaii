7d664d07519c192b0d658d1786c492fa4ff9bbd3
Russell Vea <rbvea@hawaii.edu>
2012-01-20 23:45:15 -1000

    made toggling work, also made map a global variable

--- action.js
+++ action.js
@@ -1,3 +1,44 @@
 function setOverlay(obj) {
-    alert(obj.name + "=" + obj.checked);
+    
 }
+
+function toggle(obj){
+    $(obj).toggleClass("selected");
+
+    var zoom = $(obj).text();
+
+    //Switch on object text
+    switch(zoom)
+    {
+
+    case "Oahu":
+        map.setZoom(10);
+        map.panTo(oahu);
+        break;
+    case "Kauai":
+        map.setZoom(10);
+        map.panTo(kauai);
+        break;
+    case "Molokai":
+        map.setZoom(10);
+        map.panTo(molokai);
+        break;
+    case "Lanai":
+        map.setZoom(10);
+        map.panTo(lanai);
+        break;
+    case "Maui":
+        map.setZoom(10);
+        map.panTo(maui);
+        break;
+    case "Big Island":
+        map.setZoom(8);
+        map.panTo(hawaii);
+        break;
+    case "All Islands":
+        map.setZoom(7);
+        map.panTo(center);
+        break;
+    }
+}
+
