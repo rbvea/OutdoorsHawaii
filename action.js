507056abefa2ec12f0d4fcf9444add278260a68b
Tony Gaskell <thgaskell@gmail.com>
2012-01-20 22:52:05 -1000

    Simplified action.js

--- action.js
+++ action.js
@@ -1,14 +1,3 @@
 function setOverlay(obj) {
-    switch(obj.value)
-    {
-        case 'beaches':
-            overlay[0] = !overlay[0];
-            break;
-        case 'trails':
-            overlay[1] = !overlay[1];
-            break;
-        default:
-            alert('SHIT WENT WRONG WITH action.js setOverlay(obj)')
-    }
-    //alert('beaches=' + overlay[0] + ' trails=' + overlay[1]);
+    alert(obj.name + "=" + obj.checked);
 }
