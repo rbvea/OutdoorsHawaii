6812d2c7fc5a705187bc8a78c202178b4b62bbe2
Tony Gaskell <thgaskell@gmail.com>
2012-01-21 13:00:45 -1000

    Added sidebar updating functionality.

--- action.js
+++ action.js
@@ -1,30 +1,8 @@
 /*for all tabs and checkboxes and hides or shows layers.*/
 function update() {
     clear();
-    var b =  document.getElementById("beaches").checked;
-    var t =  document.getElementById("trails").checked;
-    $('li').each(function(index) {
-	if( $(this).hasClass("selected") ){
-	    if(index == 0) {   //ALL	
-		for(var i = 0; i < beaches.length; i++) {
-		    if (b) {
-			beaches[i].show();
-		    } 
-		    if (t) {
-			trails[i].show();
-		    } 
-		}
-		return;
-	    } else  {  //SELECT
-		if (b) {
-		    beaches[index - 1].show();
-		} 	
-		if (t) {
-		    trails[index - 1].show();
-		} 
-	    }
-	}
-    });
+    updateOverlay();
+    updateSidebar();
     findCenter();
 }
 
@@ -34,9 +12,50 @@ function clear() {
     for(var i = 0; i < beaches.length; i++){
 	beaches[i].hide();
 	trails[i].hide();
+	$(isles[i]).each(function() {
+	    $(this).style.display = 'none';
+	});
     }
 }
 
+function updateOverlay() {
+    var b =  document.getElementById("beaches").checked;
+    var t =  document.getElementById("trails").checked;
+    $('li').each(function(index) {
+	if( $(this).hasClass("selected") ){
+	    if(index == 0) {   //ALL	
+		for(var i = 0; i < beaches.length; i++) {
+		    if (b) {
+			beaches[i].show();
+			$(this.value).each(function() {
+			    $(this).style.display = 'block';
+			});
+		    } 
+		    if (t) {
+			trails[i].show();
+			$(this.value).each(function() {
+			    $(this).style.display = 'block';
+			});
+		    } 
+		}
+		return;
+	    } else  {  //SELECT
+		if (b) {
+		    beaches[index - 1].show();
+		    $(this.value).each(function() {
+			$(this).style.display = 'block';
+		    });
+		} 	
+		if (t) {
+		    trails[index - 1].show();
+		    $(this.value).each(function() {
+		        $(this).style.display = 'block';
+		    });
+		} 
+	    }
+	}
+    });
+}
 
 /* */
 function toggle(obj) {
