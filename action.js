fd56fdaaad3eab47c0d927c1b965c79d3a629bb5
Tony Gaskell <thgaskell@gmail.com>
2012-01-24 06:45:48 -1000

    Reworked functions to work with the new query.js implemented functions.     - Modified update(), now adds and removes island names from the islands array.         • query.js : updateLayers() now dynamically updates the points based on the names in the islands array.     - Removed unnecessary loop. The showing/hiding of layers is done by query.js: updateLayers(); Removed clear() because it is unnecessary with the new updateLayers() function.

--- action.js
+++ action.js
@@ -1,49 +1,41 @@
-/*for all tabs and checkboxes and hides or shows layers.*/
+/* Checkbox variables */
+var showBeaches;
+var showTrails;
+
+/* Removes and adds island names to the island array
+   Adjusts the map's viewport
+   Updates layer visibility based on selected checkboxes*/
 function update() {
-    clear();
-    var b =  document.getElementById("beaches").checked;
-    var t =  document.getElementById("trails").checked;
+    showBeaches = document.getElementById("beaches").checked;
+    showTrails = document.getElementById("trails").checked;
     $('li').each(function(index) {
 	if( $(this).hasClass("selected") ){
-	    if(index == 0) {   //ALL	
-		for(var i = 0; i < beaches.length; i++) {
-		    if (b) {
-			beaches[i].show();
-		    } 
-		    if (t) {
-			trails[i].show();
-		    } 
-		}
-		return;
+	    if(index == 0) {
+		islands = ["kauai", "oahu", "molokai", "lanai", "maui", "hawaii"];
+		return false;	// Break out of $.each() loop
 	    } else  {  //SELECT
-		if (b) {
-		    beaches[index - 1].show();
-		} 	
-		if (t) {
-		    trails[index - 1].show();
-		} 
+		if ($.inArray($(this).attr("id"), islands) < 0) {
+		    islands.push($(this).attr("id"));
+		}
+	    }
+	}
+	else {
+	    if (index > 0) {
+		if ($.inArray($(this).attr("id"), islands) > 0) {
+		    islands.splice(islands.indexOf($(this).attr("id")), 1);
+		}
 	    }
 	}
     });
     findCenter();
+    updateLayers();
 }
 
-
-
-function clear() {
-    for(var i = 0; i < beaches.length; i++){
-	beaches[i].hide();
-	trails[i].hide();
-    }
-}
-
-
-/* */
+/* Controls the CSS for tabs */
 function toggle(obj) {
     $(obj).toggleClass("selected");
     if( $(obj).text() == "All Islands") {
 	if($(obj).hasClass("selected")){
-	    clear();
 	    $("li").each(function(i) {
 		if(i != 0) {
 		    $(this).removeClass("selected");
