dd37cd18a1be7cba3e1a1a93e0e907653146b826
rbvea <rbvea@hawaii.edu>
2012-08-04 22:30:53 -1000

    refreshing with arcgis

--- rbvea.js
+++ rbvea.js
@@ -1,6 +1,5 @@
-var url = 'http://rbvea.isa-geek.com:8888/v2/outdoorshawaii/{z}/{x}/{y}.png';
-
-//var jsonUrl = 'http://192.168.1.2:7777/tiles/metadata.json';
+//var url = 'http://localhost:8888/v2/outdoorshawaii/{z}/{x}/{y}.png';
+var url =  'http://{s}.tile.cloudmade.com/02e10ae557e042ab9d012ef400178054/997/256/{z}/{x}/{y}.png';
 
 var center = new L.LatLng(21.460737,-157.997818);
 
@@ -12,11 +11,25 @@ var bounds = new L.LatLngBounds(SW, NE);
 var tileLayer = new L.TileLayer(url);
 
 var map = new L.Map('map', {
-    minZoom: 8,
+    minZoom: 10,
     center: center, 
-    zoom: 8, 
+    zoom:   10 , 
     maxBounds: bounds,
 });
 
+/*
 map.addLayer(tileLayer)
-   .setView(center, 9);
+   .setView(center, 9);*/
+
+var esriLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer")
+map.addLayer(esriLayer);
+
+var parks = new L.TileLayer.ESRI('http://tiles.arcgis.com/tiles/tNJpAOha4mODLkXz/arcgis/rest/services/Parks/MapServer');
+map.addLayer(parks);
+
+
+/*
+var layer = new L.TileLayer.ESRI('http://tiles1.arcgis.com/tiles/tNJpAOha4mODLkXz/arcgis/rest/services/Parks/MapServer/');
+
+map.addLayer(layer);
+*/
