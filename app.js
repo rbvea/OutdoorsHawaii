004a46dc8decd58da80d282be5ffd754b9d247f4
Russell Vea <rbvea@hawaii.edu>
2012-08-11 18:55:50 -1000

    adding map

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 
 var app = express();
 
-var url =  'http://{s}.tile.cloudmade.com/02e10ae557e042ab9d012ef400178054/997/256/{z}/{x}/{y}.png';
-
 app.get('/', function(req, res) {
     fs.readFile('index.html', function(err, data) {
         if(err) throw err;
