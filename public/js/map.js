7a51d3aa5db04c3a507d472b0fce404deba62090
Russell Vea <rbvea@hawaii.edu>
2012-08-23 16:27:06 -1000

    constricted maps via person's location

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,63 @@
 var center =  new L.LatLng(21.460737,-157.997818);
-var found = false;
 var map;
 
+
+function ParksCtrl($scope) {
+   $scope.parks = [];
+   $scope.addToMap = function(id, name, lng, lat) {
+       $scope.parks.push({"id": id, "name" : name, "lng": lng, "lat": lat});
+   }
+}
+
 function success(position) {
+
+    var bounds = 0.02;
+
+    var geo = "" + (position.coords.longitude - bounds) + ",";
+    geo +=  position.coords.latitude - bounds + ",";
+    geo +=  position.coords.longitude + bounds + ",";
+    geo +=  position.coords.latitude + bounds;
+
+    var testGet = $.get(testUrl, 
+                        {
+                            where : '1=1',
+                            objectIds: null,
+                            geometry: geo,
+                            geometryType: 'esriGeometryEnvelope',
+                            inSR: "4326",
+                            spatialRel: 'esriSpatialRelIntersects',
+                            outFields: '*',
+                            returnGeometry: true,
+                            maxAllowableOffset: null,
+                            geometryPrecision: null,
+                            outSR: '4326',
+                            returnIdsOnly: false,
+                            returnCountOnly: false,
+                            orderByFields: null,
+                            groupByFieldsForStatistics: null,
+                            outStatistics: null,
+                            f: 'json',
+                            token: null,
+                        },function(data) {
+                            var parks = $.parseJSON(data);
+                            L.marker([position.coords.latitude, position.coords.longitude]).addTo(map).bindPopup("You are here!").openPopup();
+                            console.log(parks);
+
+                            for(var i in parks.features) {
+                                var park = parks.features[i]; 
+                                
+                                var newItem = {"id": park.attributes.OBJECTID, "name": park.attributes.NAME, "lat": park.geometry.y, "lng": park.geometry.x};
+                                
+                                $scope.parks.push(newItem);
+
+                                L.marker([park.geometry.y,park.geometry.x])
+                                    .bindPopup('<div class="park_popup"/><h3>' + park.attributes.NAME + '</h3><p>' + park.attributes.FULLADDR)
+                                    .addTo(map);
+                            }
+                        });
+
+
+
     map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
     map.setZoom(13);
 }
@@ -33,49 +88,4 @@ map.addLayer(esriLayer);
 
 var testUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/0/query';
 
-var parks;
 
-function parks($scope) {
-    
-}
-
-var testGet = $.get(testUrl, 
-                    {
-                        where : '1=1',
-                        objectIds: null,
-                        geometry: null,
-                        geometryType: 'esriGeometryEnvelope',
-                        inSR: "4326",
-                        spatialRel: 'esriSpatialRelIntersects',
-                        outFields: '*',
-                        returnGeometry: true,
-                        maxAllowableOffset: null,
-                        geometryPrecision: null,
-                        outSR: '4326',
-                        returnIdsOnly: false,
-                        returnCountOnly: false,
-                        orderByFields: null,
-                        groupByFieldsForStatistics: null,
-                        outStatistics: null,
-                        f: 'json',
-                        token: null,
-                    },function(data) {
-                        var parks = $.parseJSON(data);
-                        for(var i in parks.features) {
-                            var park = parks.features[i];
-                            
-                            L.marker([park.geometry.y,park.geometry.x])
-                            /*
-                                .on('click', function(e){
-                                  var marker = this;
-                                  var popup = marker.bindPopup('<div class="park_popup"/><h3>' + park.attributes.NAME + '</h3><p>' + park.attributes.FULLADDR);
-                                  marker.openPopup();
-                                  $('.leaflet-popup-close-button').click(function(){
-                                    map.removeLayer(popup._popup);
-                                  })
-                                })
-                                `*/
-                                .addTo(map);
-                            
-                        }
-                   });
