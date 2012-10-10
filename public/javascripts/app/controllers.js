f4d3d78ff41da8686a57450e5fbc82077bfbffe9
rbvea <rbvea@hawaii.edu>
2012-10-09 22:09:22 -1000

    fixing broken controllers.js

--- public/javascripts/app/controllers.js
+++ public/javascripts/app/controllers.js
@@ -1,25 +1,55 @@
+var testUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/0/query';
+var infoUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/2/query';
 var feature_attributes = ['ADACOMPLY', 'BASKETBALL','BUSSTOP','COMGARDEN','EXERCISEFLD','FOOTBALL','HIKING','LIGHTING','OUTCANOE','PLAYGROUND','RESTROOM','SHOWER','SOCCER','TENNIS','BASEBALL','BOATING','CAMPING','DRINKWATER','FISHING','GOLFING','JOGGING','MTBCYCLE','PICIC','RECVEHICLE','SHADETREE','SKATEBOARD','SWIMMING','VOLLEYBALL'];
 
-function mapCtrl($rootScope, $http) {
-    
-    if(navigator.geolocation) {
-        }, function() {
-            console.log('error');
+
+function parksCtrl($scope, $http) {
+    $http.get('parks/init').success(function(data) {
+        $scope.parks = data;
+    });
+};
+
+function mapCtrl($scope, $http) {
+    var config = {
+        params: {
+            where : '1=1',
+            objectIds: null,
+            geometry: null,
+            geometryType: 'esriGeometryEnvelope',
+            inSR: "4326",
+            spatialRel: 'esriSpatialRelIntersects',
+            outFields: '*',
+            returnGeometry: true,
+            maxAllowableOffset: null,
+            geometryPrecision: null,
+            outSR: '4326',
+            returnIdsOnly: false,
+            returnCountOnly: false,
+            orderByFields: null,
+            groupByFieldsForStatistics: null,
+            outStatistics: null,
+            f: 'json',
+            token: null, 
+            callback: 'JSON_CALLBACK',
+        },
+    };
+
+    $http.jsonp(testUrl, config)
+        .success(function(data) {
+            console.log(data.features);
+            $scope.parks = data.features;
+        })
+        .error(function (e) {
+            console.log(e);
         });
-    }
+    
+    $scope.current_park = null;
 
-
-    $rootScope.center =  {
+    $scope.center =  {
             lat: 21.460737,  // initial map center latitude
             lng: -157.9978180, // initial map center longitude
         };
-
-    $rootScope.zoom = 10;
-    
-    $rootScope.selectPark = function(park) {
-        $rootScope.current_park = park;
-    }
-
+    $scope.zoom = 10;
 }
 
 function optionsCtrl($scope, $http) {
