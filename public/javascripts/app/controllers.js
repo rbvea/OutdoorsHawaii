4a465e0491139e9acc905adeda6a46cd8d5833e6
rbvea <rbvea@hawaii.edu>
2012-10-09 22:03:41 -1000

    implemented html5 location, zooming and such

--- public/javascripts/app/controllers.js
+++ public/javascripts/app/controllers.js
@@ -1,44 +1,13 @@
-var testUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/0/query';
-var infoUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/2/query';
 var feature_attributes = ['ADACOMPLY', 'BASKETBALL','BUSSTOP','COMGARDEN','EXERCISEFLD','FOOTBALL','HIKING','LIGHTING','OUTCANOE','PLAYGROUND','RESTROOM','SHOWER','SOCCER','TENNIS','BASEBALL','BOATING','CAMPING','DRINKWATER','FISHING','GOLFING','JOGGING','MTBCYCLE','PICIC','RECVEHICLE','SHADETREE','SKATEBOARD','SWIMMING','VOLLEYBALL'];
 
 function mapCtrl($rootScope, $http) {
-    var config = {
-        params: {
-            where : '1=1',
-            objectIds: null,
-            geometry: null,
-            geometryType: 'esriGeometryEnvelope',
-            inSR: "4326",
-            spatialRel: 'esriSpatialRelIntersects',
-            outFields: '*',
-            returnGeometry: true,
-            maxAllowableOffset: null,
-            geometryPrecision: null,
-            outSR: '4326',
-            returnIdsOnly: false,
-            returnCountOnly: false,
-            orderByFields: null,
-            groupByFieldsForStatistics: null,
-            outStatistics: null,
-            f: 'json',
-            token: null, 
-            callback: 'JSON_CALLBACK',
-        },
-    };
-
-    $http.jsonp(testUrl, config)
-        .success(function(data) {
-            angular.forEach(data.features, function(park) {
-                park.marker = new L.marker([park.geometry.y, park.geometry.x]).addTo($rootScope.map);
-            });
-
-            $rootScope.parks = data.features;
-            $rootScope.current_park = null;
-        })
-        .error(function (e) {
-            console.log(e);
+    
+    if(navigator.geolocation) {
+        }, function() {
+            console.log('error');
         });
+    }
+
 
     $rootScope.center =  {
             lat: 21.460737,  // initial map center latitude
@@ -46,10 +15,11 @@ function mapCtrl($rootScope, $http) {
         };
 
     $rootScope.zoom = 10;
-}
+    
+    $rootScope.selectPark = function(park) {
+        $rootScope.current_park = park;
+    }
 
-function selectPark(park) {
-    console.log(park);
 }
 
 function optionsCtrl($scope, $http) {
--- public/javascripts/app/directives.js
+++ public/javascripts/app/directives.js
@@ -1,5 +1,8 @@
+var testUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/0/query';
+var infoUrl = 'http://services.arcgis.com/tNJpAOha4mODLkXz/ArcGIS/rest/services/Parks/FeatureServer/2/query';
+
 var directives = angular.module('outdoorshi.dirs', []).
-    directive('leaflet', function($rootScope) {
+    directive('leaflet', function($rootScope, $http) {
         return {
             restrict: 'E',
             template: '<div id="map" class="angular-leaflet-map"></div>',
@@ -19,10 +22,59 @@ var directives = angular.module('outdoorshi.dirs', []).
 
                 $rootScope.map = map;
                 $rootScope.markers = [];
-            },
-        };
+
+                $rootScope.$watch('current_park', function(old, changed) {
+                    changed.marker.addTo($rootScope.map);
+                });
+
+                
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    map.setView([position.coords.latitude , position.coords.longitude], 15);
+
+                    var bounds = 0.02;
+                    var geo = position.coords.longitude - bounds + ",";
+                    geo +=  position.coords.latitude - bounds + ",";
+                    geo +=  position.coords.longitude + bounds + ",";
+                    geo +=  position.coords.latitude + bounds; 
+
+                    var config = {
+                        params: {
+                            where : '1=1',
+                            objectIds: null,
+                            geometry: geo, 
+                            geometryType: 'esriGeometryEnvelope',
+                            inSR: "4326",
+                            spatialRel: 'esriSpatialRelIntersects',
+                            outFields: '*',
+                            returnGeometry: true,
+                            maxAllowableOffset: null,
+                            geometryPrecision: null,
+                            outSR: '4326',
+                            returnIdsOnly: false,
+                            returnCountOnly: false,
+                            orderByFields: null,
+                            groupByFieldsForStatistics: null,
+                            outStatistics: null,
+                            f: 'json',
+                            token: null, 
+                            callback: 'JSON_CALLBACK',
+                        },
+                    };
+
+
+                    $http.jsonp(testUrl, config)
+                        .success(function(data) {
+                            angular.forEach(data.features, function(park) {
+                                park.marker = new L.marker([park.geometry.y, park.geometry.x]).addTo(map);
+                            });
+                            $rootScope.parks = data.features;
+                        })
+                        .error(function (e) {
+                            console.log(e);
+                        });
+                });
+            }
+        }
     });
 
-
-
-
+    
